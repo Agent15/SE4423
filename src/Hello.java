64649cd326412659eef95c4c1ef00f58883210ba
Stephen Key <skey2@uco.edu>
2022-11-14 14:42:08 -0600

    fixed a bug

--- src/Hello.java
+++ src/Hello.java
@@ -5,8 +5,8 @@ public class Hello {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Hello world!");
+		System.out.println("I fixed the bug!");
 
 	}
 
